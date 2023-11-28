9686c200c7bfcf8dd658f88646b85075651bbb66
janaali17 <ja2200300@tkh.edu.eg>
2023-11-28 18:20:25 +0200

    Update proj.py

--- proj.py
+++ proj.py
@@ -2,7 +2,7 @@
 print("Welcome to morse code translation application") #this is gonna print a welcome message to the user when he runs the code
 while True: #this while loop is used so that the applications keeps running until the user chooses 0 to exit
     options = input("enter 1 to encrypt from english to morse code, 2 to decrypt from morse code to english, and 0 to exit the application: ") #this asks the user to enter an input (Option) to start the process
-    if options == "1": #if user enter "1"
+    if options == "1": #if user enter "1" the commands below runs encryption translation
         english_to_morse_dict = { #this is the morse code, each english letter or number or symbol is represented in a specific dots and dashes in morses 
             'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
             'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
@@ -13,10 +13,10 @@ while True: #this while loop is used so that the applications keeps running unti
             ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.',
             '$': '...-..-', '@': '.--.-.', ' ': '/'}
         text = input("enter a text to encrypt: ") #asks user to eneter input
-        text = " ".join(english_to_morse_dict.get(char, "") for char in text.upper())
-        print("your encyrpted text is : " ,text)
-    elif options == "2": 
-        morse_to_english_dict = { #if the user clicks 2 then the application is going to decrypt
+        encrypted_message = " ".join(english_to_morse_dict.get(char, "") for char in text.upper())
+        print("your encyrpted text is : " ,encrypted_message) #this prints "your encyrpted text is: " then the encyrpted_message
+    elif options == "2": #the below commands run if the user enters "2" decryption translation
+        morse_to_english_dict = { 
             '.-': 'A', '-...': 'B', '-.-.': 'C', '-..': 'D', '.': 'E', '..-.': 'F', '--.': 'G', '....': 'H',
             '..': 'I', '.---': 'J', '-.-': 'K', '.-..': 'L', '--': 'M', '-.': 'N', '---': 'O', '.--.': 'P',
             '--.-': 'Q', '.-.': 'R', '...': 'S', '-': 'T', '..-': 'U', '...-': 'V', '.--': 'W', '-..-': 'X',
@@ -25,8 +25,8 @@ while True: #this while loop is used so that the applications keeps running unti
             '..--..': '?', '.----.': "'", '-.-.--': '!', '-..-.': '/', '-.--.': '(', '-.--.-': ')', '.-...': '&',
             '---...': ':', '-.-.-.': ';', '-...-': '=', '.-.-.': '+', '-....-': '-', '..--.-': '_', '.-..-.': '"',
             '...-..-': '$', '.--.-.': '@', '/': ' '}
-        text = input("enter a text to decrypt: ")
-        decrypted_message = "".join(morse_to_english_dict.get(char, "") for char in text.split()) #this is to joi
+        text = input("enter a text to decrypt: ") #asks the user 
+        decrypted_message = "".join(morse_to_english_dict.get(char, "") for char in text.split()) 
         print("your decyrpted text is : " ,decrypted_message)
     elif options == "0":
         break #this is to exit the app
